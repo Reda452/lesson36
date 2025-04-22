8f449089d35436798e272945c74e1b838b35527a
Reda452 <redasakhi0@gmail.com>
2025-04-22 20:40:39 +0100

    New

---
+++ activity1.py
@@ -0,0 +1,12 @@
+#Importing necessary libraries
+from tkinter import *
+
+#Craete window
+w= Tk()
+
+#Set the window's Title and geometry
+w.title("demo window")
+w.geometry("400x300")
+
+w.mainloop()
+
